d4420526296a26a9311ca7ca503d48c5c2b74019
radrys <radoslaw.rys@wp.pl>
2025-12-11 22:44:20 +0100

    chore: scaffold application with Gradle and Kotlin

---
+++ src/main/kotlin/ai/forge/app/codegen_lab/CodegenLabApplication.kt
@@ -0,0 +1,11 @@
+package ai.forge.app.codegen_lab
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CodegenLabApplication
+
+fun main(args: Array<String>) {
+	runApplication<CodegenLabApplication>(*args)
+}
